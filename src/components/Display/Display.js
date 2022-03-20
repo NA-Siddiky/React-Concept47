3f65b05dddc1ed8529e951aaef253a18a01c034b
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-20 18:04:13 +0600

    check props

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Dial from '../Dial/Dial';
 
 const Display = (props) => {
+    console.log(props);
     return (
-        <div style={{border: '2px solid green', margin: '5px'}}>
+        <div style={{ border: '2px solid green', margin: '5px' }}>
             <h2>Name: {props.name}</h2>
             <p>So far steps today: {props.steps}</p>
             <Dial steps={props.steps}></Dial>
